05ebb1c85af91aedfc0966d5c35ef52c1ac1f77f
_alvin_ <santito.sfg@gmail.com>
2024-03-30 00:11:51 -0400

    comentando algunas lineas

--- forEjemplo.py
+++ forEjemplo.py
@@ -4,7 +4,8 @@ lista = ["Alvin", 19, 'Masculino']
 
 # operador "in" hace referencia a que pertenece
 #haremos que imprima todos los datos de la lista con for y while
-# ACLAREMOS QUE DATO YA SE CONVERTIRA EN EL VALOR DE LA LISTA NO ES UN INDICE DE RECORRIDO
+# ACLAREMOS QUE DATO YA SE CONVERTIRA EN EL VALOR DE LA LISTA NO 
+#ES UN INDICE DE RECORRIDO
 for dato in lista:
     print(dato)
 
@@ -14,14 +15,15 @@ while c < len(lista):
     print(lista[c]); c+=1
 
 
-
+print("imprimiendo números de 0 a 9 xd")
 for i in range(0, 10):
     print(i)
 
 #tamaño de la lista
-print(f"\n{len(lista)}")
+print(f"La lista es \n{len(lista)}")
 
 #imprimiendo toda la lista haciendo uso de range y el for
-#en el range sobreentiende que iniciará desde 0, por eso solo se le da el parámetro tope
-for j in range(len(lista)):
-    print(lista[j])
+#en el range sobreentiende que iniciará desde 0, por eso 
+#solo se le da el parámetro tope
+# for j in range(len(lista)):
+#     print(lista[j])
