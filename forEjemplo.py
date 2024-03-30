23198c91ed24e92f315d8d635f1f73501c987d98
Alvin :) <126908757+KraAlv@users.noreply.github.com>
2024-03-30 00:01:38 -0400

    subida codigo python "ejemplos del ciclo for"

---
+++ forEjemplo.py
@@ -0,0 +1,27 @@
+#creamos lista de referencia
+
+lista = ["Alvin", 19, 'Masculino']
+
+# operador "in" hace referencia a que pertenece
+#haremos que imprima todos los datos de la lista con for y while
+# ACLAREMOS QUE DATO YA SE CONVERTIRA EN EL VALOR DE LA LISTA NO ES UN INDICE DE RECORRIDO
+for dato in lista:
+    print(dato)
+
+print("\nMetodo con while\n")
+c=0
+while c < len(lista):
+    print(lista[c]); c+=1
+
+
+
+for i in range(0, 10):
+    print(i)
+
+#tamaño de la lista
+print(f"\n{len(lista)}")
+
+#imprimiendo toda la lista haciendo uso de range y el for
+#en el range sobreentiende que iniciará desde 0, por eso solo se le da el parámetro tope
+for j in range(len(lista)):
+    print(lista[j])
